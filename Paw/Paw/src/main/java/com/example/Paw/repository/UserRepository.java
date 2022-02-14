f25529705b930c761564aea12362d8f448a33d28
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2022-02-14 11:21:20 -0600

    UPDATE

--- Paw/Paw/src/main/java/com/example/Paw/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Paw.repository;
-
-import com.example.Paw.service.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
